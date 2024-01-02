dea46250e662a9dc47e3fc75118fee52b414ad89
Swapnil Nichal <swapnilnichal10@gmail.com>
2024-01-02 13:28:43 +0530

    updated db related options

--- server/util/db.js
+++ server/util/db.js
@@ -3,7 +3,11 @@ require('dotenv').config();
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(`mongodb+srv://swapnilnichal10:${process.env.DB_PASSWORD}@cluster0.3panst6.mongodb.net/FitEarn?retryWrites=true&w=majority`);
+    await mongoose.connect(`mongodb+srv://swapnilnichal10:${process.env.DB_PASSWORD}@cluster0.3panst6.mongodb.net/FitEarn?retryWrites=true&w=majority`,{
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000,
+    });
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
